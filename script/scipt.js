e983d4a37bbb3c2dfb85d4db4d4c4b26190fa324
JCGViljoen <jcgviljoen51@gmail.com>
2023-10-27 12:27:47 +0200

    added js

--- script/scipt.js
+++ script/scipt.js
@@ -1,4 +1,41 @@
-function generateTimeline(events) {
+document.addEventListener("DOMContentLoaded", function () {
+  const testimonialCards = document.querySelectorAll(".testimonial-card");
+  const prevButton = document.getElementById("prevTestimonial");
+  const nextButton = document.getElementById("nextTestimonial");
+  let currentTestimonial = 0;
+
+  function showTestimonial(index) {
+    testimonialCards.forEach((card, i) => {
+      if (i === index) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  }
+
+  function showNextTestimonial() {
+    currentTestimonial++;
+    if (currentTestimonial >= testimonialCards.length) {
+      currentTestimonial = 0;
+    }
+    showTestimonial(currentTestimonial);
+  }
+
+  function showPrevTestimonial() {
+    currentTestimonial--;
+    if (currentTestimonial < 0) {
+      currentTestimonial = testimonialCards.length - 1;
+    }
+    showTestimonial(currentTestimonial);
+  }
+
+  showTestimonial(currentTestimonial);
+
+  prevButton.addEventListener("click", showPrevTestimonial);
+  nextButton.addEventListener("click", showNextTestimonial);
+
+  function generateTimeline(events) {
     let timelineHtml = '';
     events.forEach(event => {
       timelineHtml += `
@@ -13,19 +50,19 @@ function generateTimeline(events) {
     });
     return timelineHtml;
   }
-  
+
   const events = [
     { date: '2010-2014', title: 'Bachelor of Science in Computer Science', description: 'University of XYZ' },
     { date: '2014-2016', title: 'Software Engineer', description: 'ABC Company' },
     { date: '2016-2020', title: 'Senior Software Engineer', description: 'DEF Company' },
   ];
-  
+
   const timelineContainer = document.getElementById('timeline-container');
   timelineContainer.innerHTML = generateTimeline(events);
-  
+
   // Get all the timeline events
   const timelineEvents = document.querySelectorAll('.timeline-event');
-  
+
   // Create a new Intersection Observer
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -38,8 +75,9 @@ function generateTimeline(events) {
       }
     });
   });
-  
+
   // Observe each timeline event
   timelineEvents.forEach(event => {
     observer.observe(event);
   });
+});
